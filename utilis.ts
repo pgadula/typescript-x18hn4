e232473c7a2d401d18da2b2d49cb4684ef6d5642
pgadula <98215353+pgadula@users.noreply.github.com>
2023-06-09 16:43:17 +0200

    3 files modified, 1 added

--- utilis.ts
+++ utilis.ts
@@ -1,7 +1,9 @@
+import { vec3 } from 'wgpu-matrix';
+
 function getRandomUnitVector(normal) {
   let direction = vec3.random();
   const d = vec3.dot(normal, direction);
-  const s = sign(d);
+  const s = d;
   direction = vec3.mul(normal, vec3.mulScalar(direction, s));
   return vec3.normalize(direction);
 }
