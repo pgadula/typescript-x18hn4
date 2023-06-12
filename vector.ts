e096329986a89383eedcff8eeef5f01614c06315
pgadula <98215353+pgadula@users.noreply.github.com>
2023-06-12 20:57:35 +0200

    1 file deleted

--- vector.ts
+++
@@ -1,7 +0,0 @@
-function getRandomUnitVector(normal) {
-  let direction = vec3.random();
-  const d = vec3.dot(normal, direction);
-  const s = sign(d);
-  direction = vec3.mul(normal, vec3.mulScalar(direction, s));
-  return vec3.normalize(direction);
-}
